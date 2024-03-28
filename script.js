e3922551e824abceeba4b33c3a8c8b2bcdbe0b5f
Jihu Choe <me@jihuchoe.com>
2024-03-28 15:37:51 +0900

    업데이트

--- script.js
+++
@@ -1,6 +0,0 @@
-let counter = document.querySelector('.dday');
-let today = new Date(),
-    dday = new Date("May 14, 2024, 00:00:00").getTime(),
-    gap = dday - today,
-    result = Math.ceil(gap / (1000 * 60 * 60 * 24));
-counter.innerHTML = result;
